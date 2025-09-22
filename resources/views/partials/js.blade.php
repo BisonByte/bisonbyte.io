64228d7540362ec8aeb3c4bed7d495a7cfb87174
BisonByte <blackshainorg@gmail.com>
2025-09-22 15:35:12 -0400

    Primer commit: estado actual del sitio

---
+++ resources/views/partials/js.blade.php
@@ -0,0 +1,27 @@
+		<!-- Optional JavaScript _____________________________  -->
+
+		<!-- jQuery first, then Bootstrap JS -->
+		<!-- jQuery -->
+		<script src="{{ URL::asset('assets/vendor/jquery.min.js')}}"></script>
+		<!-- Bootstrap JS -->
+		<script src="{{ URL::asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+		<!-- WOW js -->
+		<script src="{{ URL::asset('assets/vendor/wow/wow.min.js')}}"></script>
+		<!-- Slick Slider -->
+		<script src="{{ URL::asset('assets/vendor/slick/slick.min.js')}}"></script>
+		<!-- Fancybox -->
+		<script src="{{ URL::asset('assets/vendor/fancybox/fancybox.umd.js')}}"></script>
+		<!-- Lazy -->
+		<script src="{{ URL::asset('assets/vendor/jquery.lazy.min.js')}}"></script>
+		<!-- js Counter -->
+		<script src="{{ URL::asset('assets/vendor/jquery.counterup.min.js')}}"></script>
+		<script src="{{ URL::asset('assets/vendor/jquery.waypoints.min.js')}}"></script>
+		<!-- validator js -->
+		<script src="{{ URL::asset('assets/vendor/validator.js')}}"></script>
+
+		<!-- Theme js -->
+		<script src="{{ URL::asset('assets/js/theme.js')}}"></script>
+	</div> <!-- /.main-page-wrapper -->
+</body>
+
+</html>
